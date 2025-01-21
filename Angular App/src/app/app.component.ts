154bca80034ddb741dceabad876ae48fdea7959e
yogesh <yogesh.rajput@pfgc.com>
2025-01-21 10:38:00 +0530

    unsubscribe demo

--- Angular App/src/app/app.component.ts
+++ Angular App/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyHeaderComponent } from './my-header/my-header.component';
 import { MyBodyComponent } from './my-body/my-body.component';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RestFreeApiService } from './services/rest-free-api.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,16 @@ import { RestFreeApiService } from './services/rest-free-api.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   data:any
+  private mySubscription: Subscription = new Subscription();
+  //data$: any =[]
   constructor(private restsvr:RestFreeApiService){}
+  ngOnDestroy(): void {
+    this.mySubscription.unsubscribe();
+  }
   ngOnInit(): void {
-    this.restsvr.getRestData().subscribe(x=>{
+    this.mySubscription = this.restsvr.getRestData().subscribe(x=>{
       this.data = x
     })
   }
